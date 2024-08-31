ffc3479f4981aa13cde88459cda53ad9570eda06
unknown <oscarandreshernandezpineda@gmail.com>
2024-08-31 12:27:37 -0500

    Building API + routes

--- api/index.js
+++ api/index.js
@@ -1,7 +1,32 @@
 import express from 'express';
+import userRoutes from './routes/user.route.js';
+
+
+// const mysql = require('mysql');
+
+// db = mysql.createConnection({
+//   host: 'localhost',
+//   database: 'stockin',
+//   user: 'root',
+//   password: ''
+// });
+
+// db.connect(err => {
+//   if (err) {
+//     console.error('Error when connecting to database');
+//   } else {
+//     console.log('Successful connection')
+//   }
+// });
+
+// module.exports = {
+//   db
+// };
 
 const app = express();
 
 app.listen(3000, () => {
-  console.log('Server listening on port 3000')
+  console.log("Server listening on port http://localhost:3000")
 });
+
+app.use("/api/user", userRoutes);
