34c75dd3f35790060c2fe77f87a97b54b0301f8a
Camilo Avendaño <garaven.camilo@gmail.com>
2024-09-04 14:17:36 -0500

    StockIn logo with and w/o text to svg

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,10 +1,12 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
+import { Nav } from './components/Nav.jsx'
 import './index.css'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    {/* <App /> */}
+    <Nav></Nav>
   </StrictMode>,
 )
