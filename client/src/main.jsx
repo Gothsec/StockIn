df22e080cb26fe9ddc44c93a78dfc78a52d2d186
Camilo Avendaño <garaven.camilo@gmail.com>
2024-09-04 14:21:49 -0500

    Removing unused component

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,12 +1,10 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
-import { Nav } from './components/Nav.jsx'
 import './index.css'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    {/* <App /> */}
-    <Nav></Nav>
+    <App />
   </StrictMode>,
 )
