52054c2857bf56593440f47d8e6fec5eb5547dec
Camilo Avendaño <garaven.camilo@gmail.com>
2024-09-15 15:00:58 -0500

    Added GET '/read-order' endpoint to 'endPoints' file

--- server/api/endPoints.js
+++ server/api/endPoints.js
@@ -4,10 +4,16 @@ import { login } from '../controllers/loginController.js';
 import { readProduct } from '../controllers/product/readProductsController.js';
 import { addProduct } from '../controllers/product/addProductController.js';
 import { removeProduct } from '../controllers/product/removeProductController.js';
+import { readOrder } from '../controllers/order/readOrderController.js';
 
 router.post('/login', login);
+
+// products
 router.get('/read-product', readProduct)
 router.post('/add-product', addProduct)
 router.delete('/remove-product/:id', removeProduct)
+
+// orders
+router.get('/read-order', readOrder)
 
 export default router;
