1c8eaa53dcfb5c19a5f6007afcddad48a5a97629
Jhon Alex Rodriguez Benitez <rodriguezjhon2090@gmail.com>
2024-09-17 01:17:08 -0500

    [modification] added validations and modified modalWindows.

--- client/src/components/ModalWindows.jsx
+++ client/src/components/ModalWindows.jsx
@@ -5,25 +5,76 @@ function ModalWidows({ open, onClose, titleModal, buttonText, onClickFunction })
   const [precioPublico, setPrecioPublico] = useState("");
   const [precioCosto, setPrecioCosto] = useState("");
   const [newProductName, setNewProductName] = useState("");
-  const [codigo, setCodigo] = useState("");
   const [estadoProducto, setEstadoProducto] = useState("activo");
-  const [categoria, setCategoria] = useState("");
+  const [categoria, setCategoria] = useState("maquinas");
   const [cantidadMinima, setCantidadMinima] = useState("");
   const [marca, setMarca] = useState("");
   const [bodega, setBodega] = useState("");
   const [proveedor, setProveedor] = useState("");
+  const [errorMessages, setErrorMessages] = useState([]);
+
+  const deleteInputs = () => {
+    setNewProductName("");
+    setStock("");
+    setPrecioPublico("");
+    setPrecioCosto("");
+    setCantidadMinima("");
+    setMarca("");
+    setBodega("");
+    setProveedor("");
+    setErrorMessages([]);
+  }
+
+  const validateForm = () => {
+    let errors = [];
+  
+    // Validar campos vacíos
+    const isEmptyField = [
+      newProductName.trim(),
+      stock.trim(),
+      precioCosto.trim(),
+      precioPublico.trim(),
+      estadoProducto.trim(),
+      cantidadMinima.trim(),
+      marca.trim()
+    ].some(field => field === '');
+  
+    if (isEmptyField) {
+      errors.push("¡Debes de rellenar todos los campos!");
+    }
+  
+    // Validar valores negativos
+    if (Number(stock) < 0) errors.push("La cantidad inicial no puede ser negativa.");
+    if (Number(precioCosto) < 0) errors.push("El precio costo no puede ser negativo.");
+    if (Number(precioPublico) < 0) errors.push("El precio público no puede ser negativo.");
+    if (Number(cantidadMinima) < 0) errors.push("La cantidad mínima no puede ser negativa.");
+  
+    // Validar cantidad mínima vs cantidad inicial
+    if (Number(cantidadMinima) > Number(stock)) errors.push("La cantidad mínima no puede ser mayor que la cantidad inicial.");
+  
+    // Validar precio costo vs precio público
+    if (Number(precioCosto) > Number(precioPublico)) errors.push("El precio costo no puede ser mayor que el precio público.");
+  
+    return errors;
+  };
+
+  const handleBtnCancel = (e) => {
+    deleteInputs();
+    onClose();
+  }
 
   const handleAddProduct = (e) => {
-    e.preventDefault();
+    if (e) e.preventDefault();
 
-    if (newProductName.trim() === "") {
-      console.log("Please enter a product name");
-      return;
+    const validationErrors = validateForm();
+    setErrorMessages(validationErrors);
+
+    if (validationErrors.length > 0){
+      return; // Se cancela creación
     }
 
     // Convertir valores a números si es necesario
     const newProduct = {
-      codigo: parseInt(codigo, 10), // Asegúrate de que el código sea un número
       nombre: newProductName,
       stock: parseFloat(stock), // Convertir a número
       precioCosto: parseFloat(precioCosto), // Convertir a número
@@ -47,16 +98,7 @@ function ModalWidows({ open, onClose, titleModal, buttonText, onClickFunction })
       .then(response => response.json())
       .then(result => {
         if (result.message === 'Product added successfully') {
-          setNewProductName("");
-          setStock("");
-          setPrecioPublico("");
-          setPrecioCosto("");
-          setCodigo("");
-          setCategoria("");
-          setCantidadMinima("");
-          setMarca("");
-          setBodega("");
-          setProveedor("");
+          deleteInputs();
           onClickFunction(e); // Ejecuta la función pasada como prop
           onClose(); // Cierra el modal
         }
@@ -76,9 +118,20 @@ function ModalWidows({ open, onClose, titleModal, buttonText, onClickFunction })
 
   return (
     <div className="w-screen h-screen fixed top-0 left-0 bg-black bg-opacity-50 p-10 flex items-center justify-center">
-      <div className="w-[600px] min-h-[100px] bg-white relative rounded-lg shadow-lg p-5">
+      <div className="w-[800px] min-h-[100px] bg-white relative rounded-lg shadow-lg p-6">
         <h1 className="text-xl font-bold mb-4"> {titleModal} </h1>
-        <div className="grid grid-cols-3 gap-4">
+          {/* Muestra los mensajes de error si existen */}
+          {errorMessages.length > 0 && (
+            <div className="mb-4 p-4 bg-red-100 border border-red-400 text-red-700 rounded">
+              <ul>
+                {errorMessages.map((error, index) => (
+                  <li key={index}>{error}</li>
+                ))}
+              </ul>
+            </div>
+          )}
+
+        <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-5 gap-4">
           <div className="my-4 col-span-3">
             <label className="block text-sm font-medium text-gray-700">Nombre</label>
             <input 
@@ -89,17 +142,7 @@ function ModalWidows({ open, onClose, titleModal, buttonText, onClickFunction })
               required 
             />
           </div>
-          <div className="my-4 col-span-2">
-            <label className="block text-sm font-medium text-gray-700">Código</label>
-            <input 
-              type="number" 
-              className="w-full mt-1 p-2 border border-gray-300 rounded" 
-              value={codigo}
-              onChange={(e) => setCodigo(e.target.value)}
-              required 
-            />
-          </div>
-          <div className="my-4">
+          <div className="my-4 col-span-1">
             <label className="block text-sm font-medium text-gray-700">Cantidad inicial</label>
             <input 
               type="number" 
@@ -119,7 +162,7 @@ function ModalWidows({ open, onClose, titleModal, buttonText, onClickFunction })
               required 
             />
           </div>
-          <div className="my-4">
+          <div className="my-4 col-span-1">
             <label className="block text-sm font-medium text-gray-700">Precio público</label>
             <input 
               type="number" 
@@ -129,7 +172,7 @@ function ModalWidows({ open, onClose, titleModal, buttonText, onClickFunction })
               required 
             />
           </div>
-          <div className="my-4">
+          <div className="my-4 col-span-1">
             <label className="block text-sm font-medium text-gray-700">Estado</label>
             <select 
               className="w-full mt-1 p-2 border border-gray-300 rounded" 
@@ -140,17 +183,21 @@ function ModalWidows({ open, onClose, titleModal, buttonText, onClickFunction })
               <option value="inactivo">Inactivo</option>
             </select>
           </div>
-          <div className="my-4 col-span-2">
+          <div className="my-4 col-span-1">
             <label className="block text-sm font-medium text-gray-700">Categoría</label>
-            <input 
-              type="text" 
+            <select 
               className="w-full mt-1 p-2 border border-gray-300 rounded" 
               value={categoria}
               onChange={(e) => setCategoria(e.target.value)}
-              required 
-            />
+            >
+              <option value="maquinas">Maquinas</option>
+              <option value="ropa interior">Ropa interior</option>
+              <option value="cuidado capilar">Cuidado capilar</option>
+              <option value="cremas">Cremas</option>
+              <option value="utileria">Utileria</option>
+            </select>
           </div>
-          <div className="my-4">
+          <div className="my-4 col-span-1">
             <label className="block text-sm font-medium text-gray-700">Cantidad mínima</label>
             <input 
               type="number" 
@@ -160,7 +207,7 @@ function ModalWidows({ open, onClose, titleModal, buttonText, onClickFunction })
               required 
             />
           </div>
-          <div className="my-4">
+          <div className="my-4 col-span-1">
             <label className="block text-sm font-medium text-gray-700">Marca</label>
             <input 
               type="text" 
@@ -170,7 +217,7 @@ function ModalWidows({ open, onClose, titleModal, buttonText, onClickFunction })
               required 
             />
           </div>
-          <div className="my-4">
+          <div className="my-4 col-span-1">
             <label className="block text-sm font-medium text-gray-700">Bodega</label>
             <input 
               type="text" 
@@ -179,7 +226,7 @@ function ModalWidows({ open, onClose, titleModal, buttonText, onClickFunction })
               onChange={(e) => setBodega(e.target.value)}
             />
           </div>
-          <div className="my-4">
+          <div className="my-4 col-span-1">
             <label className="block text-sm font-medium text-gray-700">Proveedor</label>
             <input 
               type="text" 
@@ -188,7 +235,7 @@ function ModalWidows({ open, onClose, titleModal, buttonText, onClickFunction })
               onChange={(e) => setProveedor(e.target.value)}
             />
           </div>
-          <div className="my-4">
+          <div className="my-4 col-span-1">
             <label className="block text-sm font-medium text-gray-700">Ganancia</label>
             <input 
               type="number" 
@@ -197,7 +244,7 @@ function ModalWidows({ open, onClose, titleModal, buttonText, onClickFunction })
               readOnly 
             />
           </div>
-          <div className="my-4">
+          <div className="my-4 col-span-1">
             <label className="block text-sm font-medium text-gray-700">Stock total</label>
             <input 
               type="number" 
@@ -216,7 +263,7 @@ function ModalWidows({ open, onClose, titleModal, buttonText, onClickFunction })
             </div>
           ) : (
             <div className="flex justify-between">
-              <button onClick={onClose} className="bg-gray-300 text-black py-2 px-4 rounded">
+              <button onClick={handleBtnCancel} className="bg-gray-300 text-black py-2 px-4 rounded">
                 Cancelar
               </button>
               {buttonText === "Crear" ? (
