963a54ecf996bb651dcd13cc9746427d3df337e7
Gothsec <oscarandreshernandezpineda@gmail.com>
2024-10-05 16:41:15 -0500

    [Refactor] Orders page list with supabase

--- client/src/components/orders/OrderRow.jsx
+++ client/src/components/orders/OrderRow.jsx
@@ -1,37 +1,91 @@
-import React, { useState } from "react";
-import ReadUpdateOrderModal from "./ReadUpdateOrderModal";
+import { useState } from "react";
+import supabase from "../../utils/supabase";
+import ModalWindows from "./ModalWindows";
 
-export function OrderRow({ name, id, className, onDelete }) {
-  const [isModalOpen, setIsModalOpen] = useState(false);
-  const [modalOption, setModalOption] = useState("read");
+export default function OrderRow({ description, id, className, onUpdate }) {
+  const [modalProps, setModalProps] = useState({
+    titleModal: "",
+    buttonText: "",
+    onClickFunction: () => {},
+    orderInfo: {},
+  });
+  const [windowsModal, setWindowsModal] = useState(false);
 
-  const handleDeleteOrder = () => {
-    onDelete(id);
-    fetch(`http://localhost:3000/delete-order/${id}`, {
-      method: "PATCH",
-      headers: {
-        "Content-Type": "application/json",
-      },
-    })
-      .then((response) => {
-        if (response.ok) {
-          // Aquí deberías actualizar el estado de la lista de pedidos
-        } else {
-          console.error("No se pudo eliminar el pedido.");
-        }
-      })
-      .catch((err) => {
-        console.error("Error: ", err);
-      });
+  const abrirCerrarModal = (
+    titleModal,
+    buttonText,
+    onClickFunction,
+    orderInfo
+  ) => {
+    setModalProps({
+      titleModal,
+      buttonText,
+      onClickFunction,
+      orderInfo,
+    });
+    setWindowsModal(!windowsModal);
   };
 
-  const openModal = (option) => {
-    setModalOption(option);
-    setIsModalOpen(true);
+  const handleDelete = async () => {
+    const { error } = await supabase
+      .from("order")
+      .update({ state: false })
+      .eq("id", id);
+
+    if (error) {
+      console.error("Error al actualizar el estado del pedido: ", error);
+    } else {
+      if (onUpdate) onUpdate();
+    }
   };
 
-  const closeModal = () => {
-    setIsModalOpen(false);
+  const getOrderById = async (id, option) => {
+    if (!id) {
+      console.error("No se proporcionó un ID de producto válido.");
+      return;
+    }
+
+    const { data, error } = await supabase
+      .from("order")
+      .select("*")
+      .eq("id", id)
+      .single();
+
+    if (error) {
+      console.error("Error al obtener información del pedido: ", error);
+      return;
+    }
+
+    if (option === "update") {
+      abrirCerrarModal(
+        "Información Pedido",
+        "Modificar",
+        () => handleUpdate(data),
+        data
+      );
+    } else {
+      abrirCerrarModal("Información Producto", "Mostrar", () => {}, data);
+    }
+  };
+
+  const handleUpdate = async (orderData) => {
+    if (!id) {
+      console.error("ID de pedido no está definido para la actualización.");
+      return;
+    }
+
+    const { error } = await supabase
+      .from("order")
+      .update(orderData)
+      .eq("id", id);
+
+    if (error) {
+      console.error("Error actualizando pedido: ", error);
+    } else {
+      if (onUpdate) onUpdate();
+    }
+
+    abrirCerrarModal("", "", () => {}, {});
   };
 
   return (
@@ -40,32 +94,37 @@ export function OrderRow({ name, id, className, onDelete }) {
         id={`${id}`}
         className={`flex justify-between items-center w-full ${className}`}
       >
-        <td className="p-3">{name}</td>
+        <td className="p-3">{description}</td>
         <td className="flex justify-between p-3 w-[25%]">
           <button
             className="py-1 px-2 bg-red-500 text-white rounded-md"
-            onClick={handleDeleteOrder}
+            onClick={handleDelete}
           >
             Eliminar
           </button>
           <button
             className="py-1 px-2 bg-green-500 text-white rounded-md"
-            onClick={() => openModal("update")}
+            onClick={() => getOrderById(id, "update")}
           >
             Editar
           </button>
           <button
             className="py-1 px-2 bg-blue-500 text-white rounded-md"
-            onClick={() => openModal("read")}
+            onClick={() => getOrderById(id, "read")}
           >
             Info
           </button>
         </td>
       </tr>
-
-      {isModalOpen && (
-        <ReadUpdateOrderModal option={modalOption} id={id} onClose={closeModal} />
-      )}
+      <ModalWindows
+        open={windowsModal}
+        onClose={() => abrirCerrarModal("", "", () => {}, {})}
+        titleModal={modalProps.titleModal}
+        buttonText={modalProps.buttonText}
+        onClickFunction={modalProps.onClickFunction}
+        orderInfo={modalProps.orderInfo}
+        orderId={id}
+      />
     </>
   );
 }
--- client/src/components/products/ProductRow.jsx
+++ client/src/components/products/ProductRow.jsx
@@ -1,5 +1,5 @@
-import { useState } from "react";
 import supabase from "../../utils/supabase";
+import { useState } from "react";
 import ModalWindows from "./ModalWindows";
 
 export default function ProductRow({ name, id, className, onUpdate }) {
