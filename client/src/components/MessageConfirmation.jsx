aef0ffb637e9cf96d65392e019ce3ba901080149
Camilo Avendaño <garaven.camilo@gmail.com>
2024-10-17 10:39:48 -0500

    [Add] Add validation for create product, order and supplier - [Change] Change position and z-index to error notification

--- client/src/components/MessageConfirmation.jsx
+++ client/src/components/MessageConfirmation.jsx
@@ -8,9 +8,14 @@ function MessageConfirmation() {
 
   return (
     <div
-      className={`mt-2 p-2 rounded text-white ${
+      className={`fixed top-10 right-10 p-4 rounded-md text-white ${
         notification.type === "success" ? "bg-green-500" : "bg-red-500"
       }`}
+      style={{
+        zIndex: 9999,  // Aseguramos que esté por encima del modal
+        position: "absolute",
+        transition: "all 0.3s ease-in-out",
+      }}
     >
       {notification.message}
     </div>
