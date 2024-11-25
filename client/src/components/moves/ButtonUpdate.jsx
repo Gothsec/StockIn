20c5cf49662138e4f46120ea5c2e2af429d41441
1Orchestratooor1 <rodriguezjhon2090@gmail.com>
2024-11-25 17:37:54 -0500

    [Add] add functions reset and update

--- client/src/components/moves/ButtonUpdate.jsx
+++ client/src/components/moves/ButtonUpdate.jsx
@@ -17,6 +17,15 @@ export default function ButtonUpdate({
   let changeProduct = false;
   let changeQuantity = false;
   let changeWarehouse = false;
+  let changeType = false;
+  let productChange = 0;
+  let quantityChange = 0;
+  let warehouseChange = 0;
+  let typeChange = "";
+  let productUpdate = 0;
+  let quantityUpdate = 0;
+  let warehouseUpdate = 0;
+  let typeUpdate = "";
 
   const validateMove = async () => {
     if (
@@ -55,15 +64,65 @@ export default function ButtonUpdate({
 
     if (productMoveOld != moveUpdated.id_product) {
       changeProduct = true;
+      productChange = productMoveOld;
+      productUpdate = moveUpdated.id_product;
+    } else {
+      changeProduct = false;
+      productChange = productMoveOld;
+      productUpdate = productMoveOld;
     }
     if (quantityMoveOld != moveUpdated.quantity) {
       changeQuantity = true;
+      quantityChange = quantityMoveOld;
+      quantityUpdate = moveUpdated.quantity;
+    } else {
+      changeQuantity = false;
+      quantityChange = quantityMoveOld;
+      quantityUpdate = quantityMoveOld;
     }
     if (warehouseMoveOld != moveUpdated.id_warehouse) {
       changeWarehouse = true;
+      warehouseChange = warehouseMoveOld;
+      warehouseUpdate = moveUpdated.id_warehouse;
+    } else {
+      changeWarehouse = false;
+      warehouseChange = warehouseMoveOld;
+      warehouseUpdate = warehouseMoveOld;
     }
-
+    if (typeMoveOld != moveUpdated.type) {
+      changeType = true;
+      typeChange = typeMoveOld;
+      typeUpdate = moveUpdated.type;
+    } else {
+      changeType = false;
+      typeChange = typeMoveOld;
+      typeUpdate = typeMoveOld;
+    }
   };
+
+  const reset = (stock, quantity, changeType, typeUpdate) => {
+    if (changeType) {
+      if (typeUpdate == "Entrada") {
+        return stock + quantity;
+      } else {
+        return stock - quantity;
+      }
+    } else {
+      if (typeUpdate == "Entrada") {
+        return stock - quantity;
+      } else {
+        return stock + quantity;
+      }
+    }
+  }
+  
+  const update = (stock, quantity, typeUpdate) => {
+    if(typeUpdate == "Entrada") {
+      return stock + quantity;
+    } else {
+      return stock - quantity;
+    }
+  }
 
   const handleUpdateMove = async () => {};
 
